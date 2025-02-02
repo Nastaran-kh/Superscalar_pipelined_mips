2d6b7e87bb10109ec15406aef3f36e63a6e4b321
Nastaran <nastaran1382khaleghi@gmail.com>
2025-02-02 19:34:08 +0330

    Fixed fetch module to integrate with branch predictor

--- fetch.v
+++ fetch.v
@@ -1,67 +1,41 @@
-// module fetch(clk, reset, stallf, pcsrcd, pcsrcd2, pcbranchd,pcbranchd2, signextd, signextd2, instrf, instrf2, pcplus4f, pcf, pcd, clrbp);
-  
-//   input  clk, reset, stallf;
-//   input  [1:0] pcsrcd, pcsrcd2;
-//   input  [31:0] signextd, signextd2, pcd, pcbranchd,pcbranchd2; 
-//   output [31:0] instrf, pcplus4f, pcf, instrf2;
-//   output clrbp;
-
-//   // Internal signals of fetch module
-//   wire [31:0] pcnext, branchtoinstr;
-
-//   wire [2:0] muxselect;
-//   assign muxselect = {pcsrcd2[1], pcsrcd};
-
-//   // FETCH - Cycle 1
-//   // Instr Mem, Adder
-//   mux6  	  #(32) pcmux(pcplus4f, pcbranchd, signextd, signextd, signextd2, signextd2, muxselect, pcnext);
-//   flopenr         #(32) pcreg(clk, reset, stallf, pcnext, pcf);
-//   globalbranchpredictor branchpredictor(clk, pcsrcd,pcsrcd2, pcd, pcbranchd, pcbranchd2,{pcf,pcf+32'h4}, branchtoinstr, clrbp);
-//   adder   	        pcadder(branchtoinstr, 32'h8, pcplus4f);
-//   instrmem              instructmem(branchtoinstr, instrf,instrf2);
-
-// endmodule
-
 `include "instrmem.v"
 `include "adder.v"
 `include "flopenr.v"
 `include "mux6.v"
 `include "branch_predictor.v"
-module fetch(clk, reset, stallf, pcsrcd, pcsrcd2, pcbranchd, pcbranchd2, signextd, signextd2, instrf, instrf2, pcplus4f, pcf, pcd, clrbp);
-  
-input  clk, reset, stallf;
-input  [1:0] pcsrcd, pcsrcd2;
-input  [31:0] signextd, signextd2, pcd, pcbranchd, pcbranchd2; 
-output [31:0] instrf, pcplus4f, pcf, instrf2;
-output clrbp;
 
-// Internal signals of fetch module
-wire [31:0] pcnext, branchtoinstr;
-wire predict_taken;
-
-wire [2:0] muxselect;
-assign muxselect = {pcsrcd2[1], pcsrcd};
-
-// Branch Predictor Integration
-branch_predictor #(
-    .PREDICTOR_MODE(4),  // Gshare + Bimodal
-    .GBHR_SIZE(7),
-    .BHT_SIZE(512)
-) bp (
-    .clk(clk),
-    .rst(reset),
-    .pc_in(pcf),
-    .predict_taken(predict_taken),
-    .update_en(clrbp),
-    .update_pc(pcbranchd),
-    .actual_taken(pcsrcd[0])
+module fetch(
+    input clk,
+    input reset,
+    input stallf,
+    input [1:0] pcsrcd, pcsrcd2,
+    input [31:0] signextd, signextd2, pcd, pcbranchd, pcbranchd2,
+    output [31:0] instrf, instrf2, pcplus4f, pcf,
+    output clrbp
 );
 
-// FETCH - Cycle 1
-// Instr Mem, Adder
-mux6  	  #(32) pcmux(pcplus4f, pcbranchd, signextd, signextd, signextd2, signextd2, muxselect, pcnext);
-flopenr         #(32) pcreg(clk, reset, stallf, pcnext, pcf);
-adder   	        pcadder(branchtoinstr, 32'h8, pcplus4f);
-instrmem              instructmem(branchtoinstr, instrf, instrf2);
+    // Internal signals of fetch module
+    wire [31:0] pcnext, branchtoinstr;
+    wire predict_taken;
+
+    wire [2:0] muxselect;
+    assign muxselect = {pcsrcd2[1], pcsrcd};
+
+    // Branch Predictor Integration
+    branch_predictor bp (
+        .clk(clk),
+        .reset(reset),
+        .pc(pcbranchd),              // Pass the branch address
+        .branch_taken(pcsrcd[0]),    // Actual outcome of branch (taken or not)
+        .instruction(instrf),        // Current instruction
+        .predict(predict_taken)      // Predicted outcome (taken or not)
+    );
+
+    // FETCH - Cycle 1
+    // Instruction Memory, Adder
+    mux6 #(32) pcmux(pcplus4f, pcbranchd, signextd, signextd, signextd2, signextd2, muxselect, pcnext);
+    flopenr #(32) pcreg(clk, reset, stallf, pcnext, pcf);
+    adder pcadder(branchtoinstr, 32'h8, pcplus4f);
+    instrmem instructmem(branchtoinstr, instrf, instrf2);
 
 endmodule
--- instrmem.v
+++ instrmem.v
@@ -1,18 +1,37 @@
-// External unified memory used by MIPS multicycle processor.
-module instrmem(a, rd,rd2);
-  input [31:0] a;
-  output [31:0] rd,rd2;
+// // External unified memory used by MIPS multicycle processor.
+// module instrmem(a, rd,rd2);
+//   input [31:0] a;
+//   output [31:0] rd,rd2;
    
-  reg [31:0] RAM[63:0];
+//   reg [31:0] RAM[63:0];
 
-  integer i;
+//   integer i;
 
+//   initial begin
+//     for (i=21; i<64; i=i+1)  // CHANGE START VALUE WHENEVER YOU CHANGE RAM VALUES
+//       RAM[i] = 32'b0;
+//     $readmemh("memfile.dat",RAM); // "memfile.dat" contains your instructions in hex
+//   end
+
+//   assign rd = {RAM[a[31:2]]}; // word aligned
+//   assign rd2 ={RAM[a[31:2]+30'h1]};
+// endmodule
+module instrmem (
+  input [31:0] addr,       // آدرس ورودی
+  output reg [31:0] instr  // دستور خروجی
+);
+
+  reg [31:0] mem [0:63];   // حافظه 64 کلمه‌ای
+
+  // خواندن داده از فایل در آغاز شبیه‌سازی
   initial begin
-    for (i=21; i<64; i=i+1)  // CHANGE START VALUE WHENEVER YOU CHANGE RAM VALUES
-      RAM[i] = 32'b0;
-    $readmemh("memfile.dat",RAM); // "memfile.dat" contains your instructions in hex
+      // بررسی و خواندن داده‌ها از فایل
+      $readmemh("memfile.dat", mem, 0, 63);  // خواندن از فایل و ذخیره در حافظه
   end
 
-  assign rd = {RAM[a[31:2]]}; // word aligned
-  assign rd2 ={RAM[a[31:2]+30'h1]};
+  // دسترسی به حافظه بر اساس آدرس ورودی
+  always @(*) begin
+      instr = mem[addr[7:2]];  // فرض می‌کنیم addr[7:2] ایندکس حافظه است
+  end
+
 endmodule
