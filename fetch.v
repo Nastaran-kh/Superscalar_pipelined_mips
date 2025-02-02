bf4f5d9757a99c8d16174bdc1fbf39f3201a5406
Nastaran <nastaran1382khaleghi@gmail.com>
2025-02-02 19:45:26 +0330

    Fixed fetch module to integrate with branch predictor

--- fetch.v
+++ fetch.v
@@ -3,17 +3,24 @@
 `include "flopenr.v"
 `include "mux6.v"
 `include "branch_predictor.v"
-
 module fetch(
     input clk,
     input reset,
     input stallf,
-    input [1:0] pcsrcd, pcsrcd2,
-    input [31:0] signextd, signextd2, pcd, pcbranchd, pcbranchd2,
-    output [31:0] instrf, instrf2, pcplus4f, pcf,
+    input [1:0] pcsrcd,
+    input [1:0] pcsrcd2,
+    input [31:0] signextd,
+    input [31:0] signextd2,
+    input [31:0] pcd,
+    input [31:0] pcbranchd,
+    input [31:0] pcbranchd2,
+    output [31:0] instrf,
+    output [31:0] pcplus4f,
+    output [31:0] pcf,
+    output [31:0] instrf2,
     output clrbp
 );
-
+  
     // Internal signals of fetch module
     wire [31:0] pcnext, branchtoinstr;
     wire predict_taken;
@@ -25,17 +32,42 @@ module fetch(
     branch_predictor bp (
         .clk(clk),
         .reset(reset),
-        .pc(pcbranchd),              // Pass the branch address
-        .branch_taken(pcsrcd[0]),    // Actual outcome of branch (taken or not)
-        .instruction(instrf),        // Current instruction
-        .predict(predict_taken)      // Predicted outcome (taken or not)
+        .pc(pcf),        // ورودی صحیح
+        .branch_taken(predict_taken),  // شاخه واقعی گرفته شده
+        .instruction(instrf), // ورودی دستور
+        .predict(predict_taken) // پیش‌بینی شده
     );
 
     // FETCH - Cycle 1
-    // Instruction Memory, Adder
-    mux6 #(32) pcmux(pcplus4f, pcbranchd, signextd, signextd, signextd2, signextd2, muxselect, pcnext);
-    flopenr #(32) pcreg(clk, reset, stallf, pcnext, pcf);
-    adder pcadder(branchtoinstr, 32'h8, pcplus4f);
-    instrmem instructmem(branchtoinstr, instrf, instrf2);
+    // Instr Mem, Adder
+    mux6 #(32) pcmux(
+        .in0(pcplus4f), 
+        .in1(pcbranchd), 
+        .in2(signextd), 
+        .in3(signextd), 
+        .in4(signextd2), 
+        .in5(signextd2), 
+        .sel(muxselect), 
+        .out(pcnext)
+    );
+
+    flopenr #(32) pcreg(
+        .clk(clk), 
+        .reset(reset), 
+        .en(~stallf), 
+        .d(pcnext), 
+        .q(pcf)
+    );
+
+    adder pcadder(
+        .a(branchtoinstr), 
+        .b(32'h8), 
+        .sum(pcplus4f)
+    );
+
+    instrmem instructmem(
+        .addr(branchtoinstr), 
+        .instr(instrf) // خروجی صحیح
+    );
 
 endmodule
